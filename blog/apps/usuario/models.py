1bb39ec417e3c09d8f9624c726677787ab97efe3
Gonzalo <gonzafernandez94@gmail.com>
2021-12-18 17:49:26 -0300

    Update models.py

--- blog/apps/usuario/models.py
+++ blog/apps/usuario/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+#creo un models para usuario 
+class Usuario():
+	"""docstring for USsuario"""
+	def __init__(self, arg):
+		super(USU
+suario, self).__init__(
+		elf.arg = arg
