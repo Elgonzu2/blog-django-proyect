962dea7b0abf98ae0ab3cfc3bdeaa23992cfb451
Gonzalo <gonzafernandez94@gmail.com>
2021-12-18 17:52:31 -0300

    Update local.py

--- blog/blog/settings/local.py
+++ blog/blog/settings/local.py
@@ -7,7 +7,7 @@ DATABASES={
         'ENGINE':'django.db.backends.mysql',
         'NAME': 'blogdb',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '3306',
     },
