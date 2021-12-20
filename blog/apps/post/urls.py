cbcf4e4d5d2f19e149090eb5f0b795f90772b294
Gonzalo <gonzafernandez94@gmail.com>
2021-12-20 17:19:37 -0300

    Update urls.py

--- blog/apps/post/urls.py
+++ blog/apps/post/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 app_name="post"
 
@@ -9,6 +14,6 @@ urlpatterns = [
     path('addcomentario/', views.AddComentario, name="addcomentario"),
     path('toppost/', views.GetTopPost, name='toppost'),
     path('readpost/<int:id>', views.ReadPost, name="readpost"),
-    path('comentarios/', views.Comentarios, name="comentarios")
-    #path('')
+    path('comentarios/', views.Comentarios, name="comentarios"),
+    path('crearpost/', views.CrearPost.as_view(), name="crearpost"),
 ]
